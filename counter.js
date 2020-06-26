a101bce1c81c28413169ae468439ed2fcc06ba2a
ran cohen <ranco.nyc@gmail.com>
2020-06-26 22:07:36 +0300

    currency

--- counter.js
+++ counter.js
@@ -1,15 +1,26 @@
-  let counter = 0;
+  if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+  }
 
-      function count() {
-        counter++;
-        document.querySelector('h1').innerHTML = counter;
+  function count() {
+    let counter = localStorage.getItem('counter');
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter);
 
-        if (counter % 10 === 0) {
-          alert(`Count is now ${counter}`);
-        }
-      }
+    // if (counter % 10 === 0) {
+    //   alert(`Count is now ${counter}`);
+    // }
+  }
 
-      document.addEventListener('DOMContentLoaded', function() {
-        document.querySelector('button').onclick = count;
-      });
+  function zero(){
+    document.querySelector('h1').innerHTML = 0;
+    localStorage.setItem('counter', 0);
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+    document.querySelector('button').onclick = count;
+    document.querySelector('#zero').onclick = zero;
+  });
 
